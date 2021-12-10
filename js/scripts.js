eeae1909b2ebb5d53ae75388af119b8bea2b657e
Kody104 <jpk.stu@gmail.com>
2021-12-10 17:28:31 -0600

    Created the initial group of objects for the Pokedex
    Created the first 9 pokemon from the pokdex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,55 @@
-alert('Hello World');
-let favFood = "Chicken Alfredo";
-document.write(favFood);
+let pokemonList = [{
+  name: 'Bulbasaur',
+  natDexRank: 1,
+  height: 0.7,
+  weight: 6.9,
+  type: ['Grass', 'Poison']
+}, {
+  name: 'Ivysaur',
+  natDexRank: 2,
+  height: 1.0,
+  weight: 13.0,
+  type: ['Grass', 'Poison']
+}, {
+  name: 'Venusaur',
+  natDexRank: 3,
+  height: 2.0,
+  weight: 100.0,
+  type: ['Grass', 'Poison']
+}, {
+  name: 'Charmander',
+  natDexRank: 4,
+  height: 0.6,
+  weight: 8.5,
+  type: ['Fire']
+}, {
+  name: 'Charmeleon',
+  natDexRank: 5,
+  height: 1.1,
+  weight: 19.0,
+  type: ['Fire']
+}, {
+  name: 'Charizard',
+  natDexRank: 6,
+  height: 1.7,
+  weight: 90.5,
+  type: ['Fire', 'Flying']
+}, {
+  name: 'Squirtle',
+  natDexRank: 7,
+  height: 0.5,
+  weight: 9.0,
+  type: ['Water']
+}, {
+  name: 'Wartortle',
+  natDexRank: 8,
+  height: 1.0,
+  weight: 22.5,
+  type: ['Water']
+}, {
+  name: 'Blastoise',
+  natDexRank: 9,
+  height: 1.6,
+  weight: 85.5,
+  type: ['Water']
+}];
