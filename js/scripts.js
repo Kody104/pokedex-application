e64c73dab17d62747a408dba5635220e9136540b
Kody104 <jpk.stu@gmail.com>
2021-12-10 18:46:59 -0600

    Added a BMI calculation!

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,23 @@ let pokemonList = [{
   weight: 85.5,
   type: ['Water']
 }];
+
+function calculateBMI(pokemon) {
+  let cmHeight = pokemon.height * 100;
+  return ((pokemon.weight / cmHeight / cmHeight) * 10000).toFixed(1);
+}
+
+document.write('Currently added pokemon to the pokedex: ')
+for(let i = 0; i < pokemonList.length; i++) {
+  let bmi = calculateBMI(pokemonList[i]);
+  document.write('<br>' + pokemonList[i].name + ' - BMI ' + bmi);
+  if(bmi >= 25.0) {
+    document.write(' (<b>If this pokemon were human, they would be considered overweight!</b>) ');
+  }
+  else if(bmi <= 18.5) {
+    document.write(' (<b>If this pokemon were human, they would be considered underweight!</b>) ')
+  }
+  if(i !== pokemonList.length - 1) {
+    document.write(', ');
+  }
+}
